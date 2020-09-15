6ee7e22542a0c0657a423c28e6a25a14f14f5928
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-09-15 10:23:55 -0400

    Update manage-admin.php

--- Capstone-Project-master/admin/manage-admin.php
+++ Capstone-Project-master/admin/manage-admin.php
@@ -116,9 +116,10 @@ if($_SESSION['login']!="1"){
 							  $cnt=1;
 								  //loops for every user in db
 							  while($row=mysqli_fetch_array($ret))
-							  { if($row=get_value($mysqli,"SELECT admin FROM log_in")=1){ ?>
+							  { if($row=get_value($mysqli,"SELECT admin FROM log_in")==1){ ?>
                               <tr>
                               <td><?php echo $cnt;?></td>
+                              <php>
                     
                                   <td><?php echo $row['name'];?></td>
                                  <td><?php echo $row['email'];?></td>
