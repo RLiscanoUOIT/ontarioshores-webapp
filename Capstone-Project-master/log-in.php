927b4ed276edf269a2d707d724067a356866c324
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-09-22 07:37:48 -0400

    Update log-in.php

--- Capstone-Project-master/log-in.php
+++ Capstone-Project-master/log-in.php
@@ -7,6 +7,7 @@
 <head>
 <title>Login Page</title>
 <link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body style="background-color:#bdc3c7">
 	<div id="main-wrapper">
