eed23848f1bdbc3597c665ee1c77cf6db351ba86
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-10-21 09:37:15 -0400

    Update public-upload.php

--- Capstone-Project-master/public-upload.php
+++ Capstone-Project-master/public-upload.php
@@ -75,7 +75,7 @@ if(isset($_POST['upload']))
                       </a>
                   </li>
                   <li class="sub-menu">
-                      <a href="staff/public-album.php" >
+                      <a href="../public-album.php" >
                           <i class="fa fa-image"></i>
                           <span>Public Album</span>
                       </a>
