3f97ee0b1ccd8a9d525a09609fc77b30656cadff
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-10-22 15:26:56 -0400

    Update update-files_staff.php

--- Capstone-Project-master/staff/update-files_staff.php
+++ Capstone-Project-master/staff/update-files_staff.php
@@ -28,7 +28,7 @@ if(isset($_POST['upload']))
   <head>
 <html lang="en">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device=width, initial-scale=1.0">
+    <meta name="viewport" content="width-device=width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
@@ -38,6 +38,8 @@ if(isset($_POST['upload']))
     <link href="admin/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
     <link href="admin/assets/css/style.css" rel="stylesheet">
     <link href="admin/assets/css/style-responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
   </head>
 
   <body>
