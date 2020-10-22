74e9dfd1a9eb864b25b72cebb3bf9fa6191d4019
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-10-22 15:30:45 -0400

    Update private-album.php

--- Capstone-Project-master/staff/private-album.php
+++ Capstone-Project-master/staff/private-album.php
@@ -34,6 +34,8 @@ if($_SESSION['login']!="1"){
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
   </head>
 
   <body>
