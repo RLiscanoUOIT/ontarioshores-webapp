7aafcaf75960840d68fcf4050f11143a897b2cc6
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-10-22 15:23:02 -0400

    Update update-files_staff.php

--- Capstone-Project-master/staff/update-files_staff.php
+++ Capstone-Project-master/staff/update-files_staff.php
@@ -28,7 +28,7 @@ if(isset($_POST['upload']))
   <head>
 <html lang="en">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device=width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
