df15b549223f70b97e533f20ddfea2b355ce0656
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-10-22 19:12:10 -0400

    asdc

--- Capstone-Project-master/staff/private-album.php
+++ Capstone-Project-master/staff/private-album.php
@@ -107,6 +107,17 @@ if($_SESSION['login']!="1"){
                                   </div>
                                 </div>
                                 <div class="col mb-4">
+                                  <div class="card h-100 w-100">
+                                    <img src="https://os-webapp1.s3.amazonaws.com/2.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Card title</h5>
+                                      <p class="card-text"><small class="text-muted">46 Items</small>
+                                      <button class="btn btn-danger btn-s pull-right" onClick="return confirm('Do you really want to delete');"  a href="https://www.facebook.com/" >
+                                            <i class="fa fa-trash-o " alt="Delete" title="Delete"></i></button></a></p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col mb-4">
                                     <div class="card h-100 w-100">
                                       <img src="$urlstr" id="$urlstr" class="card-img-top" alt="...">
                                       <div class="card-body">
@@ -118,6 +129,17 @@ if($_SESSION['login']!="1"){
                                     </div>
                                   </div>
                                   <div class="col mb-4">
+                                  <div class="card h-100 w-100">
+                                    <img src="https://os-webapp1.s3.amazonaws.com/2.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Card title</h5>
+                                      <p class="card-text"><small class="text-muted">46 Items</small>
+                                      <button class="btn btn-danger btn-s pull-right" onClick="return confirm('Do you really want to delete');"  a href="https://www.facebook.com/" >
+                                            <i class="fa fa-trash-o " alt="Delete" title="Delete"></i></button></a></p>
+                                    </div>
+                                  </div>
+                                </div>
+                                  <div class="col mb-4">
                                     <div class="card h-100 w-100">
                                       <img src="https://os-webapp1.s3.amazonaws.com/dog.png" class="card-img-top" alt="...">
                                       <div class="card-body">
@@ -128,6 +150,17 @@ if($_SESSION['login']!="1"){
                                       </div>
                                     </div>
                                   </div>
+                                  <div class="col mb-4">
+                                  <div class="card h-100 w-100">
+                                    <img src="https://os-webapp1.s3.amazonaws.com/2.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Card title</h5>
+                                      <p class="card-text"><small class="text-muted">46 Items</small>
+                                      <button class="btn btn-danger btn-s pull-right" onClick="return confirm('Do you really want to delete');"  a href="https://www.facebook.com/" >
+                                            <i class="fa fa-trash-o " alt="Delete" title="Delete"></i></button></a></p>
+                                    </div>
+                                  </div>
+                                </div>
                                   <div class="col mb-4">
                                     <div class="card h-100 w-100">
                                       <img src="https://os-webapp1.s3.amazonaws.com/dog.png" class="card-img-top" alt="...">
