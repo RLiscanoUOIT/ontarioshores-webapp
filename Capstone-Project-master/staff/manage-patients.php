2df35b909a39cbc36fefd9aec3c8c10cfda83915
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-10-23 01:44:50 -0400

    CHANGES VIEWPORT SETTINGS

--- Capstone-Project-master/staff/manage-patients.php
+++ Capstone-Project-master/staff/manage-patients.php
@@ -28,7 +28,7 @@ if($_SESSION['login']!="1"){
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device=width, initial=scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
