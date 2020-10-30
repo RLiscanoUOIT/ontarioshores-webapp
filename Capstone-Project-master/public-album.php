6b75d211fe1f3ee7d78f9a2c6f020b83b790a97e
Angela Tabafunda <angela.tabafunda@ontariotechu.net>
2020-10-30 05:36:00 -0400

    fixed paths

--- Capstone-Project-master/public-album.php
+++ Capstone-Project-master/public-album.php
@@ -4,7 +4,7 @@ include'dbconnection.php';
 
 //ensures user is logged in
 if($_SESSION['login']!="1"){
-	header( "Location: ../log-in.php");
+	header( "Location: log-in.php");
 }
 
 ?>
@@ -30,10 +30,10 @@ if($_SESSION['login']!="1"){
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
 
     <title>Staff | Private Album</title>
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-    <link href="../admin/assets/css/style.css" rel="stylesheet">
-    <link href="assets/css/style-responsive.css" rel="stylesheet">
+    <link href="admin/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="admin/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="admin/assets/css/style.css" rel="stylesheet">
+    <link href="admin/assets/css/style-responsive.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
   </head>
@@ -59,10 +59,10 @@ if($_SESSION['login']!="1"){
           <div id="sidebar"  class="nav-collapse ">
               <ul class="sidebar-menu" id="nav-accordion">
 
-              <p class="centered"><a href="#"><img src="assets/img/logo100.png" width="125"></a></p>
+              <p class="centered"><a href="#"><img src="admin/assets/img/logo100.png" width="125"></a></p>
   	 
                   <li class="sub-menu">
-                      <a href="manage-patients.php" >
+                      <a href="staff/manage-patients.php" >
                           <i class="fa fa-users"></i>
                           <span>Manage Patients</span>
                       </a>
@@ -137,12 +137,12 @@ if($_SESSION['login']!="1"){
 
 
 
-        <script src="assets/js/jquery.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-        <script src="assets/js/jquery.scrollTo.min.js"></script>
-        <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-        <script src="assets/js/common-scripts.js"></script>
+        <script src="admin/assets/js/jquery.js"></script>
+        <script src="admin/assets/js/bootstrap.min.js"></script>
+        <script class="include" type="text/javascript" src="admin/assets/js/jquery.dcjqaccordion.2.7.js"></script>
+        <script src="admin/assets/js/jquery.scrollTo.min.js"></script>
+        <script src="admin/assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+        <script src="admin/assets/js/common-scripts.js"></script>
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     </body>
